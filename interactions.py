948ddbb6925a8074f27318023a5efc3d620d9c02
RokIvansek <ivansek.rok@gmail.com>
2016-07-14 12:56:32 +0200

    found a really good implementation of entropy

--- interactions.py
+++ interactions.py
@@ -1,8 +1,24 @@
 import numpy as np
 from Orange.data import Table
 from sklearn.utils.extmath import cartesian
+import timeit
+import itertools
+from functools import reduce
 
-def H(*rand_vars, return_prob_dist=False):
+def wrapper(func, *args, **kwargs):
+    def wrapped():
+        return func(*args, **kwargs)
+    return wrapped
+
+#Stole this from http://blog.biolab.si/2012/06/15/computing-joint-entropy-in-python/
+#It performs great.
+#TODO: Make sure the probabilities use additive smoothing (alpha = 1) t.i. Laplace probabilities
+def H(*X):
+    return np.sum(-p * np.log2(p) if p > 0 else 0 for p in
+        (np.mean(reduce(np.logical_and, (predictions == c for predictions, c in zip(X, classes))))
+            for classes in itertools.product(*[set(x) for x in X])))
+
+def H_slow(*rand_vars, return_prob_dist=False):
     """
     :param rand_vars: discrete random variables as a tuple of 1-D arrays
 
@@ -35,7 +51,8 @@ def H(*rand_vars, return_prob_dist=False):
     n = len(R) #number of instances in R
     keys, counts = np.unique(R, return_counts=True) #count occurances of unique values
     k = len(keys) #number of unique values in R
-    probs_laplace = [(c+1)/(n + k) for c in counts] #probabilities with additive smoothing (alpha=1)
+    # probs_laplace = [(c+1)/(n + k) for c in counts] #probabilities with additive smoothing (alpha=1)
+    probs_laplace = [c/n for c in counts]  # probabilities with additive smoothing (alpha=1)
     prob_dist = dict(zip(keys, probs_laplace)) #present probability distribution in the format of a dictionary
     entropy = -sum(probs_laplace*np.log2(probs_laplace)) #calculate entropy
     # print("Unique values:", keys)
@@ -67,18 +84,20 @@ def get_information_gains(data):
     n = len(data.domain.attributes)
     info_gains = {data.domain.attributes[i]: I(data.X[:,i], data.Y) for i in range(n)}
     # TODO: What is the right way to access columns of data in Orange data table?
-    for key in info_gains:
-        print(key, "info gain:", info_gains[key])
     return info_gains
 
 class Interaction:
-    def __init__(self, abs_ig_a, abs_ig_b, abs_ig_ab, class_ent):
+    def __init__(self, a_name, b_name, abs_ig_a, abs_ig_b, abs_ig_ab, class_ent):
+        self.a_name = a_name
+        self.b_name = b_name
         self.abs_ig_a = abs_ig_a
         self.abs_ig_b = abs_ig_b
         self.abs_ig_ab = abs_ig_ab
+        self.abs_total_ig_ab = abs_ig_a + abs_ig_b - abs_ig_ab
         self.rel_ig_a = self.abs_ig_a/class_ent
         self.rel_ig_b = self.abs_ig_b/class_ent
         self.rel_ig_ab = self.abs_ig_ab/class_ent
+        self.rel_total_ig_ab = self.abs_total_ig_ab/class_ent
 
     def __str__(self):
         return "Write this method so it will print multiple lines."
@@ -107,7 +126,9 @@ class Interactions:
         ig_a = I(self.data.X[:, a], self.data.Y)
         ig_b = I(self.data.X[:, b], self.data.Y)
         ig_ab = I(self.data.X[:, a], self.data.X[:, b],  self.data.Y)
-        inter = Interaction(ig_a, ig_b, ig_ab, self.class_entropy)
+        a_name = self.data.domain.attributes[a]
+        b_name = self.data.domain.attributes[b]
+        inter = Interaction(a_name, b_name, ig_a, ig_b, ig_ab, self.class_entropy)
         return inter
         #TODO
 
@@ -117,15 +138,16 @@ def load_xor_data():
     data = Table(X, Y)
     return data
 
-def load_mushrooms_data():
+def load_mushrooms_data(no_samples=False):
     shrooms_data = np.array(np.genfromtxt("./data/agaricus-lepiota.data", delimiter=",", dtype=str))
     # Convert mushroom data from strings to integers
     for i in range(len(shrooms_data[0, :])):
         u, ints = np.unique(shrooms_data[:, i], return_inverse=True)
         shrooms_data[:, i] = ints
-    #sample a smaller subset of mushrooms data
-    np.random.shuffle(shrooms_data)
-    shrooms_data = shrooms_data[:100,:]
+    if no_samples:
+        #sample a smaller subset of mushrooms data
+        np.random.shuffle(shrooms_data)
+        shrooms_data = shrooms_data[:no_samples,:]
     Y_shrooms = shrooms_data[:, 0]
     X_shrooms = shrooms_data[:, 1:]
     data = Table(X_shrooms, Y_shrooms)  # Make and Orange.Table object, without domain added
@@ -160,30 +182,70 @@ def test_I(data):
 
 def test_Interactions(data):
     #Test interactions class
-    print("All information gains of individual attributes:")
     inter = Interactions(data)
-    #Testing class Interaction and relative info gain calculation
-    a = 0
-    b = 3
-    chart_info = inter.attribute_interactions(a, b)
-    print(chart_info.rel_ig_a)
-    print(chart_info.rel_ig_b)
-    print(chart_info.rel_ig_ab)
+    print("All absolute information gains of individual attributes:")
+    for key in inter.info_gains:
+        print(key, "abs info gain:", inter.info_gains[key])
+    print("****************************************************************************")
+    print("All absolute information gains of individual attributes:")
+    for key in inter.info_gains:
+        print(key, "rel info gain:", inter.info_gains[key]/inter.class_entropy)
+    print("****************************************************************************")
+    #Print out info gain for all of the pairs of attributes
+    charts = []
+    for a in range(len(data.domain.attributes)):
+        for b in range(a+1, len(data.domain.attributes)):
+            chart_info = inter.attribute_interactions(a, b)
+            charts.append(chart_info)
+            # print("Relative info gain for attribute", data.domain.attributes[a], ":", chart_info.rel_ig_a)
+            # print("Relative info gain for attribute", data.domain.attributes[b], ":", chart_info.rel_ig_b)
+            # print("Relative info gain for attributes", data.domain.attributes[a], "and",
+            #       data.domain.attributes[b], "together", ":", chart_info.rel_ig_ab)
+            # print("Total relative info gain for attributes", data.domain.attributes[a], "and",
+            #       data.domain.attributes[b], ":", chart_info.rel_total_ig_ab)
+    charts.sort(key=lambda x: x.rel_total_ig_ab, reverse=True)
+    print("Attributes with highest total relative info gain:")
+    for i in range(10):
+        chart_info = charts[i]
+        print("Relative info gain for attribute", chart_info.a_name, ":", chart_info.rel_ig_a)
+        print("Relative info gain for attribute", chart_info.b_name, ":", chart_info.rel_ig_b)
+        print("Relative info gain for attributes", chart_info.a_name, "and",
+              chart_info.b_name, "together", ":", chart_info.rel_ig_ab)
+        print("Total relative info gain for attributes", chart_info.a_name, "and",
+              chart_info.b_name, ":", chart_info.rel_total_ig_ab)
+        print("****************************************************************************")
+    charts.sort(key=lambda x: x.rel_ig_ab)
+    print("Attributes with lowest relative info gain:")
+    for i in range(10):
+        chart_info = charts[i]
+        print("Relative info gain for attribute", chart_info.a_name, ":", chart_info.rel_ig_a)
+        print("Relative info gain for attribute", chart_info.b_name, ":", chart_info.rel_ig_b)
+        print("Relative info gain for attributes", chart_info.a_name, "and",
+              chart_info.b_name, "together", ":", chart_info.rel_ig_ab)
+        print("Total relative info gain for attributes", chart_info.a_name, "and",
+              chart_info.b_name, ":", chart_info.rel_total_ig_ab)
+        print("****************************************************************************")
 
 if __name__ == '__main__':
     # TODO: check correctnes of H and I, something is off
     # data = Table("lenses")  # Load discrete dataset
-    # data = load_mushrooms_data() # Load bigger discrete dataset
-    data = load_xor_data()
-    print(data.X)
-    print(data.Y)
-    test_H(data)
-    test_I(data)
-    # test_Interactions(data)
-    # a = np.random.randint(5, size=1000)
-    # b = np.random.randint(5, size=1000)
+    data = load_mushrooms_data() # Load bigger discrete dataset
+    # data = load_xor_data()
+    # print(data.X)
+    # print(data.Y)
+    # test_H(data)
+    # test_I(data)
+    test_Interactions(data)
+    # a = np.random.randint(5, size=100)
+    # b = np.random.randint(5, size=100)
+    # c = np.random.randint(5, size=100)
     # c = cartesian((a,b))
     # c_ = c.astype(str)
+    # wrapped = wrapper(H, a, b, c)
+    # print("H time:", timeit.timeit(wrapped, number=10))
+    # wrapped = wrapper(entropy, a, b, c)
+    # print("entropy time:", timeit.timeit(wrapped, number=10))
+    # print(H(a, b, c))
 
 
 
