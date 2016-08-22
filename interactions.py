9d16eddced711bb134eed4b5b65fcf17f74e1110
RokIvansek <ivansek.rok@gmail.com>
2016-08-22 16:12:36 +0200

    library now handles sparse arrays

--- interactions.py
+++ interactions.py
@@ -1,6 +1,7 @@
 import numpy as np
-from scipy.sparse import csr_matrix, csc_matrix, issparse
+import scipy.sparse as sp
 import Orange
+import Orange.statistics.contingency
 from itertools import chain, combinations
 import timeit
 
@@ -65,19 +66,19 @@ class Interactions:
         self.n = self.data.X.shape[1]  # TODO: What is a better way to get this two numbers m and n?
         self.m = self.data.X.shape[0]
         self.alpha = alpha
-        # TODO: Check for sparse data
         discretizer = Orange.preprocess.Discretize()
         discretizer.method = Orange.preprocess.discretize.EqualFreq(disc_intervals)
         # TODO: Should there be an option to choose the method
         # TODO: of dicscretization too? For now it is just equal frequencies on three intervals.
         # TODO: What is the proper way to make this optional, input arguments when initializing Interactions class?
         self.data = discretizer(self.data)  # Discretize continous attributes
-        self.sparse = issparse(self.data.X)  # Check for sparsity
+        self.sparse = sp.issparse(self.data.X)  # Check for sparsity
         if self.sparse: # If it is a sparse matrix, make it csc, because it enables fast column slicing operations
-            self.data.X = csc_matrix(self.data.X)
-        # self.info_gains = {self.data.domain.attributes[i]: self.i(self.data.X[:, i], self.data.Y)
-        #                    for i in range(self.n)}
-        # self.class_entropy = self.h(self.get_probs(self.data.Y))  # You will need this for relative information gain
+            self.data.X = sp.csc_matrix(self.data.X)
+            self.data.Y = sp.csc_matrix(self.data.Y.reshape((self.m,1)))  # Seems like the easiest way to convert Y too
+        self.info_gains = {self.data.domain.attributes[i]: self.i(self.data.X[:, i], self.data.Y)
+                           for i in range(self.n)}
+        self.class_entropy = self.h(self.get_probs(self.data.Y))  # You will need this for relative information gain
         self.all_pairs = [] # Here we will store the Interaction objects for all possible pairs of attributes.
 
     def get_counts_sparse(self, x, with_counts=True):
@@ -112,7 +113,7 @@ class Interactions:
 
         no_att = len(X)
         if no_att == 1:
-            if issparse(X[0]):
+            if sp.issparse(X[0]):
                 counts, uniques = self.get_counts_sparse(X[0])
                 probs = (counts + self.alpha) / (np.sum(counts) + self.alpha * len(uniques))  # additive smoothing
             else:
@@ -120,9 +121,9 @@ class Interactions:
                 uniques, counts = np.unique(x, return_counts=True)  # count
                 probs = (counts + self.alpha) / (len(x) + self.alpha * len(uniques))  # additive smoothing
         else:
-            if issparse(X[0]):
+            if sp.issparse(X[0]):
                 uniques = [self.get_counts_sparse(x, with_counts=False) for x in X]
-                M = np.column_stack((tuple([x.toarray() for x in X])))  # Get dense arrays and stack in a matrix
+                M = np.column_stack([x.toarray().flatten() for x in X])  # Get dense arrays and stack in a matrix
             else:
                 uniques = [np.unique(x[~np.isnan(x)]) for x in X]  # Unique values for each attribute column, no NaNs.
                 M = np.column_stack((X))  # Stack the columns in a matrix.
@@ -221,8 +222,10 @@ def load_artificial_data(no_att, no_samples, no_unique_values, no_classes, no_na
         np.put(Y, np.random.choice(range(no_samples), no_class_nans, replace=False), np.nan) #put in some nans
     if sparse:
         np.put(X, np.random.choice(range(no_samples*no_att), sparse, replace=False), 0)  #make the X array sparse
-        X = csr_matrix(X)
-    data = Orange.data.Table(X, Y)
+        X = sp.csr_matrix(X)
+    domain = Orange.data.Domain([Orange.data.DiscreteVariable("Attribute" + str(i)) for i in range(1, X.shape[1] + 1)],
+                                Orange.data.DiscreteVariable("Class_variable"))
+    data = Orange.data.Table(domain, X, Y)
     return data
 
 
@@ -243,7 +246,7 @@ def load_mushrooms_data(no_samples=False, random_nans_no=False, sparse=False):
     Y_shrooms = shrooms_data[:, 0]
     X_shrooms = shrooms_data[:, 1:]
     if sparse:
-        X_shrooms = csr_matrix(X_shrooms)
+        X_shrooms = sp.csr_matrix(X_shrooms)
     domain = Orange.data.Domain([Orange.data.DiscreteVariable("attribute" + str(i)) for i in range(1,X_shrooms.shape[1]+1)],
                                 Orange.data.DiscreteVariable("edible"))
     data = Orange.data.Table(domain, X_shrooms, Y_shrooms)  # Make an Orange.Table object
@@ -260,55 +263,68 @@ def wrapper(func, *args, **kwargs):
 if __name__ == '__main__':
     # Example on how to use the class interaction:
     # d = Orange.data.Table("zoo") # Load  discrete dataset.
-    # d = load_artificial_data(100, 1000, 20, 5, sparse=50000) # Load sparse dataset
-    # d = load_mushrooms_data(sparse=True) # Load bigger dataset.
-    # inter = Interactions(d) # Initialize Interactions object.
-    # Since info gain for single attributes is computed at initialization we can already look at it.
-    # To compute the interactions of all pairs of attributes we can use method interaction_matrix.
-    # We get a symmetric matrix, but the same info is also stored in a list internally.
-    # interacts_M = inter.interaction_matrix()
-    # # We can get the 3 combinations that provide the most info about the class variable by using get_top_att
-    # best_total = inter.get_top_att(3, criteria="total")
-    # for i in best_total: # Interaction objects also print nicely.
-    #     print(i)
-    #     print("*****************************************************************")
+    # d = load_artificial_data(50, 1000, 5, 3, no_nans=1000) # Load sparse dataset
+    d = load_mushrooms_data(sparse=True, random_nans_no=500) # Load bigger dataset.
+    # d = Orange.data.Table("titanic")
+    inter = Interactions(d) # Initialize Interactions object.
+    # # Since info gain for single attributes is computed at initialization we can already look at it.
+    # # To compute the interactions of all pairs of attributes we can use method interaction_matrix.
+    # # We get a symmetric matrix, but the same info is also stored in a list internally.
+    interacts_M = inter.interaction_matrix()
+    # We can get the 3 combinations that provide the most info about the class variable by using get_top_att
+    best_total = inter.get_top_att(3, criteria="total")
+    for i in best_total: # Interaction objects also print nicely.
+        print(i)
+        print("*****************************************************************")
 
     #TEST get_probs FOR SPARSE TABLE WITH NANS
-    d = load_mushrooms_data(sparse=True)  # Load bigger dataset.
-    inter = Interactions(d)  # Initialize Interactions object.
-
-    r = 2
-    s = 100
-    nans = 100
-
-    x1 = np.random.randint(r, size=s).astype(np.float32)
-    x1 = x1.reshape((s, 1))
-    np.put(x1, np.random.choice(range(len(x1)), size=nans, replace=False), np.nan)
-    x1_sparse = csr_matrix(x1)
-
-    x2 = np.random.randint(r, size=s).astype(np.float32)
-    x2 = x2.reshape((s, 1))
-    np.put(x2, np.random.choice(range(len(x1)), size=nans, replace=False), np.nan)
-    x2_sparse = csr_matrix(x2)
-
-    x3 = np.random.randint(r, size=s).astype(np.float32)
-    x3 = x3.reshape((s, 1))
-    np.put(x3, np.random.choice(range(len(x1)), size=nans, replace=False), np.nan)
-    x3_sparse = csr_matrix(x3)
+    # d = load_mushrooms_data(sparse=True)  # Load bigger dataset.
+    # inter = Interactions(d)  # Initialize Interactions object.
+    #
+    # r = 10
+    # s = 100000
+    # nans = 10
+    #
+    # x1 = np.random.randint(r, size=s).astype(np.float32)
+    # x1 = x1.reshape((s, 1))
+    # np.put(x1, np.random.choice(range(len(x1)), size=nans, replace=False), np.nan)
+    # x1_sparse = sp.csr_matrix(x1)
+    #
+    # x2 = np.random.randint(r, size=s).astype(np.float32)
+    # x2 = x2.reshape((s, 1))
+    # np.put(x2, np.random.choice(range(len(x1)), size=nans, replace=False), np.nan)
+    # x2_sparse = sp.csr_matrix(x2)
+    #
+    # x3 = np.random.randint(r, size=s).astype(np.float32)
+    # x3 = x3.reshape((s, 1))
+    # np.put(x3, np.random.choice(range(len(x1)), size=nans, replace=False), np.nan)
+    # x3_sparse = sp.csr_matrix(x3)
 
     # print(np.sort(inter.get_probs(x1, x2, x3)))
     # print(np.sort(inter.get_probs(x1_sparse, x2_sparse, x3_sparse)))
 
     #SPEED TESTING:
-    # d = load_artificial_data(100, 1000, 50, 10)
+
+    # d = Orange.data.Table("zoo")
     # inter = Interactions(d, alpha=0)
 
-    #testin sparse routines
-    wrapped = wrapper(inter.get_probs, x1, x2, x3)
-    print("time non sparse:", timeit.timeit(wrapped, number=3) / 3)
+    #TODO: On artificial data you get an error here. "ValueError: minlength must be positive"
+    # print(Orange.statistics.contingency.Discrete(d, 0))
+    # print(inter.get_probs(inter.data.X[:,0], inter.data.Y))
+    #
+    # #testing speed of contingency table
+    # wrapped = wrapper(inter.get_probs, inter.data.X[:,0], inter.data.Y)
+    # print("time my contingency:", timeit.timeit(wrapped, number=3) / 3)
+    #
+    # wrapped = wrapper(Orange.statistics.contingency.Discrete, d, 0)
+    # print("time Orange contingency:", timeit.timeit(wrapped, number=3) / 3)
 
-    wrapped = wrapper(inter.get_probs, x1_sparse, x2_sparse, x3_sparse)
-    print("time sparse:", timeit.timeit(wrapped, number=3) / 3)
+    #testing sparse routines
+    # wrapped = wrapper(inter.get_probs, x1, x2, x3)
+    # print("time non sparse:", timeit.timeit(wrapped, number=3) / 3)
+    #
+    # wrapped = wrapper(inter.get_probs, x1_sparse, x2_sparse, x3_sparse)
+    # print("time sparse:", timeit.timeit(wrapped, number=3) / 3)
 
     #testing i
     # wrapped = wrapper(inter.h, d.X[:, 0])
